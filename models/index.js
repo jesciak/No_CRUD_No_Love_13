771842be2f1dd9843f29cc68889a89dfb1a5a181
jesciak <jesciak@yahoo.com>
2022-11-13 16:42:45 -0500

    added .env file

--- models/index.js
+++ models/index.js
@@ -17,14 +17,12 @@ Category.hasMany(Product,{
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag,{
   through: ProductTag,
-  as: 'tags',
   foreignKey: 'product_id',
   onDelete: 'SET NULL'
 });
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product,{
   through: ProductTag,
-  as: 'products',
   foreignKey: 'tag_id',
   onDelete: 'SET NULL'
 })
--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -5,24 +5,39 @@ const { Category, Product } = require('../../models');
 
 router.get('/', (req, res) => {
   // find all categories
+  Category.findAll({
   // be sure to include its associated Products
+
+})
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
+  Category.findOne({
   // be sure to include its associated Products
+
+})
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create({
+
+  })
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update({ 
+
+  })
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    
+  })
 });
 
 module.exports = router;
