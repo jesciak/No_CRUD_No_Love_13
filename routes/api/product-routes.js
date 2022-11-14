dc934e254f067f985b7a0467d9e982afc4607ed4
jesciak <jesciak@yahoo.com>
2022-11-13 22:32:37 -0500

    completed tag routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -131,6 +131,20 @@ router.put('/:id', async (req, res) => {
 
 router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
-});
-
+  await Product.destroy({
+    where:{
+      id: req.params.id
+    },
+      })
+      .then((productData)=>{
+        if(!productData){
+          res.status(404).json({message:'No product with this id!'});
+          return;
+        }
+        res.json(productData);
+      })
+      .catch((err) => {
+        res.status(500).json(err);
+      })
+    });
 module.exports = router;
